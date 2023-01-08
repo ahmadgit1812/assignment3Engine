01d82de689c4bd4652c87ef933592c70f896fde0
Ahmad Essa <ahmad_e@apple.com>
2023-01-08 22:44:07 +0200

    assignment 3 changes

--- tutorial/Example1/BasicScene.cpp
+++ tutorial/Example1/BasicScene.cpp
@@ -55,7 +55,7 @@ void BasicScene::Init(float fov, int width, int height, float near, float far)
  
     material->AddTexture(0, "textures/box0.bmp", 2);
     auto sphereMesh{IglLoader::MeshFromFiles("sphere_igl", "data/sphere.obj")};
-    auto cylMesh{IglLoader::MeshFromFiles("cyl_igl","data/xcylinder.obj")};
+    auto cylMesh{IglLoader::MeshFromFiles("cyl_igl","data/zcylinder.obj")};
     auto cubeMesh{IglLoader::MeshFromFiles("cube_igl","data/cube_old.obj")};
     sphere1 = Model::Create( "sphere",sphereMesh, material);    
     cube = Model::Create( "cube", cubeMesh, material);
@@ -75,19 +75,37 @@ void BasicScene::Init(float fov, int width, int height, float near, float far)
     root->AddChild(axis[0]);
     float scaleFactor = 1; 
     cyls.push_back( Model::Create("cyl",cylMesh, material));
-    cyls[0]->Scale(scaleFactor,Axis::X);
-    cyls[0]->SetCenter(Eigen::Vector3f(-0.8f*scaleFactor,0,0));
+    cyls[0]->Scale(scaleFactor,Axis::Z);
+    cyls[0]->SetCenter(Eigen::Vector3f(0,0,-0.8f*scaleFactor));
     root->AddChild(cyls[0]);
+
+    //
+    link_len = Eigen::Vector3f(0, 0, 0.8f);
+    DISTANCE_DELTA = 0.05f;
+    ANGEL_STEPS = 10.f;
+    temp_const1 = 180.f;
+    temp_const2=3.14f;
    
     for(int i = 1;i < 3; i++)
     { 
         cyls.push_back( Model::Create("cyl", cylMesh, material));
-        cyls[i]->Scale(scaleFactor,Axis::X);   
-        cyls[i]->Translate(1.6f*scaleFactor,Axis::X);
-        cyls[i]->SetCenter(Eigen::Vector3f(-0.8f*scaleFactor,0,0));
+        cyls[i]->Scale(scaleFactor,Axis::Z);
+        cyls[i]->Translate(1.6f*scaleFactor,Axis::Z);
+        cyls[i]->SetCenter(Eigen::Vector3f(0,0,-0.8f*scaleFactor));
         cyls[i-1]->AddChild(cyls[i]);
+
+        std::shared_ptr<Model> new_axis = Model::Create("axis",coordsys,material1);
+        axis.push_back(new_axis);
+        new_axis->mode =1;
+        new_axis->Scale(4,Axis::XYZ);
+        cyls[i-1]->AddChild(new_axis);
+        new_axis->Translate(0.8f*scaleFactor, Axis::Z);
+
     }
-    cyls[0]->Translate({0.8f*scaleFactor,0,0});
+    root->Translate({0,0,0.8f*scaleFactor});
+
+    Eigen::Vector3f root_rotation_vector = Eigen::Vector3f(-1,0,0);
+    root->RotateByDegree(90,root_rotation_vector);
 
     auto morphFunc = [](Model* model, cg3d::Visitor* visitor) {
       return model->meshIndex;//(model->GetMeshList())[0]->data.size()-1;
@@ -98,13 +116,14 @@ void BasicScene::Init(float fov, int width, int height, float near, float far)
     sphere1->showWireframe = true;
     autoCube->Translate({-6,0,0});
     autoCube->Scale(1.5f);
-//    sphere1->Translate({-2,0,0});
+    sphere1->Translate({5,0,0});
 
     autoCube->showWireframe = true;
     camera->Translate(22, Axis::Z);
     root->AddChild(sphere1);
 //    root->AddChild(cyl);
     root->AddChild(autoCube);
+
     // points = Eigen::MatrixXd::Ones(1,3);
     // edges = Eigen::MatrixXd::Ones(1,3);
     // colors = Eigen::MatrixXd::Ones(1,3);
@@ -129,6 +148,9 @@ void BasicScene::Init(float fov, int width, int height, float near, float far)
     std::cout<< "edges to faces: \n" << EF.transpose() <<std::endl;
     std::cout<< "faces to edges: \n "<< EMAP.transpose()<<std::endl;
     std::cout<< "edges indices: \n" << EI.transpose() <<std::endl;
+    sphere1->Translate({0,0,0});
+    autoCube->Translate({0,0,0});
+    angel_constant = temp_const1/temp_const2;
 
 }
 
@@ -140,8 +162,10 @@ void BasicScene::Update(const Program& program, const Eigen::Matrix4f& proj, con
     program.SetUniform4f("Kdi", 0.5f, 0.5f, 0.0f, 1.0f);
     program.SetUniform1f("specular_exponent", 5.0f);
     program.SetUniform4f("light_position", 0.0, 15.0f, 0.0, 1.0f);
-//    cyl->Rotate(0.001f, Axis::Y);
     cube->Rotate(0.1f, Axis::XYZ);
+
+    if(start_IK_solver)
+       IK_CCD_SOLVER();
 }
 
 void BasicScene::MouseCallback(Viewport* viewport, int x, int y, int button, int action, int mods, int buttonState[])
@@ -179,13 +203,27 @@ void BasicScene::MouseCallback(Viewport* viewport, int x, int y, int button, int
 void BasicScene::ScrollCallback(Viewport* viewport, int x, int y, int xoffset, int yoffset, bool dragging, int buttonState[])
 {
     // note: there's a (small) chance the button state here precedes the mouse press/release event
+    bool arm_link_picked = false;
+    int prev_index=-1;
     auto system = camera->GetRotation().transpose();
     if (pickedModel) {
-        pickedModel->TranslateInSystem(system, {0, 0, -float(yoffset)});
+        for(int i=0; i<cyls.size(); i++) {
+            if (pickedModel == cyls[i]) {
+                arm_link_picked = true;
+                prev_index = i;
+            }
+        }
+
+        if(arm_link_picked)
+            pickedModel=cyls[0];
+
+        pickedModel->TranslateInSystem(system * pickedModel->GetRotation(), {0, 0, -float(yoffset)});
         pickedToutAtPress = pickedModel->GetTout();
+
+        if(arm_link_picked)
+            pickedModel=cyls[prev_index];
     } else {
-        camera->TranslateInSystem(system, {0, 0, -float(yoffset)});
-        cameraToutAtPress = camera->GetTout();
+        root->TranslateInSystem(system, {0, 0, -float(yoffset)});
     }
 }
 
@@ -197,8 +235,26 @@ void BasicScene::CursorPosCallback(Viewport* viewport, int x, int y, bool draggi
         auto angleCoeff = camera->CalcAngleCoeff(viewport->width);
         if (pickedModel) {
             //pickedModel->SetTout(pickedToutAtPress);
-            if (buttonState[GLFW_MOUSE_BUTTON_RIGHT] != GLFW_RELEASE)
-                pickedModel->TranslateInSystem(system, {-float(xAtPress - x) / moveCoeff, float(yAtPress - y) / moveCoeff, 0});
+            if (buttonState[GLFW_MOUSE_BUTTON_RIGHT] != GLFW_RELEASE) {
+                bool arm_link_picked = false;
+                int prev_index=-1;
+
+                for(int i=0; i<cyls.size(); i++) {
+                    if (pickedModel == cyls[i]) {
+                        arm_link_picked = true;
+                        prev_index = i;
+                    }
+                }
+
+                if(arm_link_picked)
+                    pickedModel=cyls[0];
+
+                pickedModel->TranslateInSystem(system * root->GetRotation(),
+                                               {-float(xAtPress - x) / moveCoeff, float(yAtPress - y) / moveCoeff, 0});
+
+                if(arm_link_picked)
+                    pickedModel=cyls[prev_index];
+            }
             if (buttonState[GLFW_MOUSE_BUTTON_MIDDLE] != GLFW_RELEASE)
                 pickedModel->RotateInSystem(system, float(xAtPress - x) / angleCoeff, Axis::Z);
             if (buttonState[GLFW_MOUSE_BUTTON_LEFT] != GLFW_RELEASE) {
@@ -252,9 +308,6 @@ void BasicScene::KeyCallback(Viewport* viewport, int x, int y, int key, int scan
             case GLFW_KEY_A:
                 camera->TranslateInSystem(system, {-0.1f, 0, 0});
                 break;
-            case GLFW_KEY_D:
-                camera->TranslateInSystem(system, {0.1f, 0, 0});
-                break;
             case GLFW_KEY_B:
                 camera->TranslateInSystem(system, {0, 0, 0.1f});
                 break;
@@ -287,8 +340,157 @@ void BasicScene::KeyCallback(Viewport* viewport, int x, int y, int key, int scan
                     tipIndex++;
                 }
                 break;
+            case GLFW_KEY_T:
+                print_arms_tip_position();
+                break;
+            case GLFW_KEY_D:
+                print_destination_position();
+                break;
+            case GLFW_KEY_N:
+                pick_next_link();
+                break;
+            case GLFW_KEY_SPACE:
+                set_IK_solver_flag();
+                break;
+            case GLFW_KEY_P:
+                print_rotation_matrix();
+                break;
         }
     }
+}
+
+void BasicScene::print_rotation_matrix(){
+    int picked_link_index=-1;
+    for(int i=0; i<cyls.size();i++)
+        if(pickedModel==cyls[i])
+            picked_link_index=i;
+
+    if(picked_link_index != -1){
+        std::cout << "link "<< picked_link_index << " is picked." << std::endl;
+        Eigen::Matrix3f picked_link_rot= cyls[picked_link_index]->GetRotation();
+
+        std::cout << "link rotation: " << std::endl << picked_link_rot << std::endl;
+        Eigen::Vector3f picked_link_angels = picked_link_rot.eulerAngles(2,0,2);
+
+        std::cout << "link matrices: " << std::endl;
+        Eigen::Matrix3f phi_matrix;
+        phi_matrix << cos(picked_link_angels(0)), -sin(picked_link_angels(0)), 2
+        , sin(picked_link_angels(0)), cos(picked_link_angels(0)), 0
+        , 0, 0, 1;
+        std::cout << "phi matrix: " << std::endl << phi_matrix << std::endl;
+
+
+        Eigen::Matrix3f theta_matrix;
+        theta_matrix << 1, 0, 0
+                , 0, cos(picked_link_angels(1)), -sin(picked_link_angels(1))
+                , 0, sin(picked_link_angels(1)), cos(picked_link_angels(1));
+        std::cout << "theta matrix: " << std::endl << theta_matrix << std::endl;
+
+
+        Eigen::Matrix3f psi_matrix;
+        psi_matrix << cos(picked_link_angels(2)), -sin(picked_link_angels(2)), 0
+                , sin(picked_link_angels(2)), cos(picked_link_angels(2)), 0
+                , 0, 0, 1;
+        std::cout << "psi matrix: " << std::endl << psi_matrix << std::endl;
+
+
+        std::cout << "link angles: " << std::endl;
+        std::cout << "phi: "<< picked_link_angels(0) * angel_constant<< std::endl;
+        std::cout << "theta: "<< picked_link_angels(1) * angel_constant<< std::endl;
+        std::cout << "psi: "<< picked_link_angels(2) * angel_constant<< std::endl;
+
+
+    }else{
+        std::cout << "scene rotation matrix: " << std::endl << root->GetRotation() << std::endl;
+    }
+}
+
+void BasicScene::set_IK_solver_flag(){
+    dest_sphere_position = sphere_position();
+    Eigen::Vector3f arm_source_position = link_source_position(0);
+    //distance between arm's source and dest center
+    float arm_sphere_distance = (dest_sphere_position - arm_source_position).norm();
+    if(1.6f * cyls.size() >= arm_sphere_distance)
+        start_IK_solver = true;
+    else
+        std::cout << "cannot reach" << std::endl;
+}
+
+bool BasicScene::arms_tip_reached_dest(){
+    arm_tip_position = link_tip_position(cyls.size()-1);
+    //distance between arm's tip and dest center
+    float arm_sphere_distance = (dest_sphere_position-arm_tip_position).norm();
+    if(arm_sphere_distance < DISTANCE_DELTA){
+        start_IK_solver=false;
+        std::cout<<"destination reached, arm <-> sphere distance: "<<arm_sphere_distance<<std::endl;
+        return true;
+    }
+    return false;
+}
+
+void BasicScene::IK_CCD_SOLVER(){
+    for(int i = cyls.size()-1; i>=0; i--){
+
+        if(arms_tip_reached_dest())
+            return;
+
+        Eigen::Vector3f curr_link_source_position = link_source_position(i);
+        Eigen::Vector3f dest_source_vec = (dest_sphere_position - curr_link_source_position).normalized();
+        Eigen::Vector3f tip_source_vec = (arm_tip_position - curr_link_source_position).normalized();
+        float dot_product = dest_source_vec.dot(tip_source_vec);
+        dot_product = dot_product > 1 ? 1 : dot_product < -1 ? -1 : dot_product ;
+        float curr_angel = acosf(dot_product);
+        curr_angel = curr_angel / ANGEL_STEPS;
+        Eigen::Vector3f curr_plane_normal = tip_source_vec.cross(dest_source_vec);
+        Eigen::Vector3f rot_vec = (cyls[i]->GetRotation().transpose() * curr_plane_normal).normalized();
+        cyls[i]->Rotate(curr_angel, rot_vec);
+    }
+}
+
+void BasicScene::pick_next_link(){
+    int curr_picked_link_index = -1;
+    for(int i=0; i<cyls.size(); i++)
+        if(pickedModel == cyls[i])
+            curr_picked_link_index=i;
+
+    int next_link_index = curr_picked_link_index == -1 ? 0 : (curr_picked_link_index+1) % cyls.size();
+    std::cout << "next link index is: " << next_link_index << std::endl;
+    pickedModel = cyls[next_link_index];
+}
+
+void BasicScene::print_destination_position(){
+    Eigen::Vector3f dest_sphere_position = sphere_position();
+    std::cout << "destination position is: "<< "("<< dest_sphere_position(0) << ", "<<
+    dest_sphere_position(1)<< ", " << dest_sphere_position(2) << ")" << std::endl;
+
+}
+
+Eigen::Vector3f BasicScene::sphere_position(){
+    Eigen::MatrixXf sphereAggregatedTransform = sphere1->GetAggregatedTransform();
+    return Eigen::Vector3f(sphereAggregatedTransform(0,3), sphereAggregatedTransform(1, 3), sphereAggregatedTransform(2,3));
+}
+
+void BasicScene::print_arms_tip_position(){
+    for(int i=0; i<cyls.size(); i++) {
+        Eigen::Vector3f curr_link_tip_position = link_tip_position(i);
+        std::cout << "link " << i << " tip position is: " << "(" << curr_link_tip_position(0) << ", "
+        << curr_link_tip_position(1) << ", "<< curr_link_tip_position(2) << ")" << std::endl;
+    }
+}
+
+Eigen::Vector3f BasicScene::link_tip_position(int index){
+    Eigen::Vector3f rotation_v = cyls[index]->GetRotation() * link_len;
+    return link_center_position(index) + rotation_v;
+}
+
+
+Eigen::Vector3f BasicScene::link_center_position(int index){
+    Eigen::Matrix4f linkAggregatedTransform = cyls[index]->GetAggregatedTransform();
+    return Eigen::Vector3f(linkAggregatedTransform(0,3), linkAggregatedTransform(1,3), linkAggregatedTransform(2,3));
+}
+Eigen::Vector3f BasicScene::link_source_position(int index){
+    Eigen::Vector3f rotation_v = cyls[index]->GetRotation() * link_len;
+    return link_center_position(index) - rotation_v;
 }
 
 Eigen::Vector3f BasicScene::GetSpherePos()
@@ -298,3 +500,9 @@ Eigen::Vector3f BasicScene::GetSpherePos()
       res = cyls[tipIndex]->GetRotation()*l;   
       return res;  
 }
+
+
+
+
+
+
--- tutorial/Example1/BasicScene.h
+++ tutorial/Example1/BasicScene.h
@@ -15,14 +15,37 @@ public:
     void ScrollCallback(cg3d::Viewport* viewport, int x, int y, int xoffset, int yoffset, bool dragging, int buttonState[]) override;
     void CursorPosCallback(cg3d::Viewport* viewport, int x, int y, bool dragging, int* buttonState)  override;
     void KeyCallback(cg3d::Viewport* viewport, int x, int y, int key, int scancode, int action, int mods) override;
-     Eigen::Vector3f GetSpherePos();
+    void print_arms_tip_position();
+    void print_destination_position();
+    void pick_next_link();
+    void IK_CCD_SOLVER();
+    void set_IK_solver_flag();
+    bool arms_tip_reached_dest();
+    void print_rotation_matrix();
+    Eigen::Vector3f sphere_position();
+    Eigen::Vector3f link_tip_position(int index);
+    Eigen::Vector3f link_center_position(int index);
+    Eigen::Vector3f link_source_position(int index);
+    Eigen::Vector3f GetSpherePos();
+
+
 private:
+    Eigen::Vector3f dest_sphere_position;
+    Eigen::Vector3f arm_tip_position;
     std::shared_ptr<Movable> root;
     std::shared_ptr<cg3d::Model> sphere1 ,cube;
     std::shared_ptr<cg3d::AutoMorphingModel> autoCube;
     std::vector<std::shared_ptr<cg3d::Model>> cyls, axis;
+    Eigen::Vector3f link_len;
+    bool start_IK_solver = false;
     int pickedIndex = 0;
     int tipIndex = 0;
+    float DISTANCE_DELTA;
+    float ANGEL_STEPS;
+    float temp_const1;
+    float temp_const2;
+    float angel_constant;
+
     Eigen::VectorXi EMAP;
     Eigen::MatrixXi F,E,EF,EI;
     Eigen::VectorXi EQ;
